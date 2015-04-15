dfd2d2643e11ebb12232f616fd92f01b6d89396f
Ben <iofelben+github@gmail.com>
2015-04-15 15:55:57 -0400

    c++ exceptions wip

--- demo.c
+++ demo.c
@@ -1,12 +1,15 @@
 #include <assert.h>
 #include <inttypes.h>
 #include <sys/types.h>
+#include <unwind.h>
+
 #include "dr_api.h"
 #include "drsyms.h"
+#include "drmgr.h"
+#include "drwrap.h"
+
 #include "kvec.h"
 
-
-/*
 char *get_sym(app_pc addr)
 {
     module_data_t *data = dr_lookup_module(addr);
@@ -31,18 +34,25 @@ char *get_sym(app_pc addr)
     }
     return NULL;
 }
-*/
+
+int tls_key = -1;
 
 void push(void *addr)
 {
-    kvec_t(void*) *stack = dr_get_tls_field(dr_get_current_drcontext());
+    kvec_t(void*) *stack = drmgr_get_tls_field(dr_get_current_drcontext(), tls_key);
     kv_push(void*, *stack, addr);
 }
 
 void *pop()
 {
-    kvec_t(void*) *stack = dr_get_tls_field(dr_get_current_drcontext());
+    kvec_t(void*) *stack = drmgr_get_tls_field(dr_get_current_drcontext(), tls_key);
     return kv_pop(*stack);
+}
+
+void *peek()
+{
+    kvec_t(void*) *stack = drmgr_get_tls_field(dr_get_current_drcontext(), tls_key);
+    return kv_A(*stack, kv_size(*stack)-1);
 }
 
 void on_call(void *call_ins, void *target_addr)
@@ -56,15 +66,16 @@ void on_ret(void *ret_ins, void *target_addr)
         ;
 }
 
-dr_emit_flags_t new_bb(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating)
+dr_emit_flags_t new_bb(void *drcontext, void *tag, instrlist_t *bb, instr_t *inst, bool for_trace, bool translating, void *user_data)
 {
-    instr_t *tail = instrlist_last(bb);
-    if (instr_is_call_direct(tail))
-        dr_insert_call_instrumentation(drcontext, bb, tail, on_call);
-    else if (instr_is_call_indirect(tail))
-        dr_insert_mbr_instrumentation(drcontext, bb, tail, on_call, SPILL_SLOT_1);
-    else if (instr_is_return(tail))
-        dr_insert_mbr_instrumentation(drcontext, bb, tail, on_ret, SPILL_SLOT_1);
+    if (inst == instrlist_last(bb)) {
+        if (instr_is_call_direct(inst))
+            dr_insert_call_instrumentation(drcontext, bb, inst, on_call);
+        else if (instr_is_call_indirect(inst))
+            dr_insert_mbr_instrumentation(drcontext, bb, inst, on_call, SPILL_SLOT_1);
+        else if (instr_is_return(inst))
+            dr_insert_mbr_instrumentation(drcontext, bb, inst, on_ret, SPILL_SLOT_1);
+    }
 
     return DR_EMIT_DEFAULT;
 }
@@ -73,28 +84,66 @@ void on_thread(void *drcontext)
 {
     kvec_t(void*) *stack = malloc(sizeof(kvec_t(void*)));
     kv_init(*stack);
-    dr_set_tls_field(drcontext, stack);
+    drmgr_set_tls_field(drcontext, tls_key, stack);
 }
 
 void on_thread_exit(void *drcontext)
 {
-    kvec_t(void*) *stack = dr_get_tls_field(drcontext);
+    kvec_t(void*) *stack = drmgr_get_tls_field(drcontext, tls_key);
     kv_destroy(*stack);
     free(stack);
 }
 
-void event_exit()
+void on_call_phase2(void *wrapctx, OUT void **user_data)
 {
+    *user_data = drwrap_get_arg(wrapctx, 1);
+}
+
+void on_ret_phase2(void *wrapctx, void *user_data)
+{
+    struct _Unwind_Context *uw = user_data;
+
+    void *catch_addr = (void*)_Unwind_GetIP(uw); // IP in catch, i.e. return address
+/*    instr_t i;
+    decode(drwrap_get_drcontext(wrapctx), catch_addr, &i);
+    void *catch_func = ; // address of function containing catch
+
+    while ( RTN_Address(RTN_FindByAddress(peek())) != catch_func )
+            pop();
+*/
+
+    push(catch_addr);
+}
+
+void on_module_load(void *drcontext, const module_data_t *info, bool loaded)
+{
+    void *addr;
+    if ((addr = dr_get_proc_address(info->handle, "_Unwind_RaiseException_Phase2")) != NULL)
+    {
+        drwrap_wrap(addr, &on_call_phase2, &on_ret_phase2);
+        drmgr_unregister_module_load_event(&on_module_load);
+    }
+}
+
+void on_exit()
+{
+    drmgr_exit();
+    drwrap_exit();
     drsym_exit();
 }
 
 DR_EXPORT void dr_init(client_id_t id)
 {
-    dr_register_exit_event(&event_exit);
-    dr_register_bb_event(&new_bb);
-    dr_register_thread_init_event(&on_thread);
-    dr_register_thread_exit_event(&on_thread_exit);
-
+    drmgr_init();
+    drwrap_init();
     drsym_init(0);
+
+    tls_key = drmgr_register_tls_field();
+
+    dr_register_exit_event(&on_exit);
+    drmgr_register_bb_instrumentation_event(NULL, &new_bb, NULL);
+    drmgr_register_module_load_event(&on_module_load);
+    drmgr_register_thread_init_event(&on_thread);
+    drmgr_register_thread_exit_event(&on_thread_exit);
 }
 
